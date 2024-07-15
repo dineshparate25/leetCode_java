1d6dc28e471b0bfd4ae1fa8e36d0f44aaee474fe
dineshparate25 <dineshparate25@gmail.com>
2024-07-15 21:37:24 +0530

    way of string

---
+++ CharAtFunction.java
@@ -0,0 +1,7 @@
+public class CharAtFunction {
+    
+    public static void main(String args[])
+    {
+        
+    }
+}
---
+++ WayOfStringCreation.java
@@ -0,0 +1,12 @@
+public class WayOfStringCreation {
+    
+    public static void main(String args[])
+    {
+        //String Literals
+        String str="Literal";
+        String str1="Literal";
+
+        System.out.println(str1);
+        System.out.println(str);
+    }
+}
